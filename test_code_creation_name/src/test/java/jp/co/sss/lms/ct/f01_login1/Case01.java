37ba46f17b8654753075bf57acf3b122159fd9f1
kss-p <kss-p@kss-pc209>
2026-09-18 15:29:25 +0900

    add:Case01.java ログイン画面への遷移テストコード追加 後藤
    WebElementを利用してログインID欄、パスワード欄、ログインボタンなどが画面上に正しく表示されているか確認するテストを作成した。
    
    期待値は以下の通り
    ・ログインID入力欄が表示されていること
    ・パスワード入力欄が表示されていること
    ・ログインボタンが有効化されていること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -2,6 +2,10 @@ package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +13,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,8 +40,18 @@ public class Case01 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		//[No.01]ログイン画面にアクセスする
+		goTo("http://localhost:8080/lms");
+		//3秒待つ
+		Thread.sleep(3000);
+
+		//エビデンスを撮る
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		Files.copy(file.toPath(), Paths.get("./evidence/Case01/No.1.png"));
+
+		System.out.println("No.1: ログイン画面確認とエビデンス保存を完了しました。");
+
 	}
 
 }
